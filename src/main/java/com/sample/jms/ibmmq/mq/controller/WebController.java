83afb80a8022110406c82fe3c2c249e718e90de6
gyuleyk <148241896+gyuleyk@users.noreply.github.com>
2023-10-19 13:21:26 +0000

    changed media type to text plain

--- src/main/java/com/sample/jms/ibmmq/mq/controller/WebController.java
+++ src/main/java/com/sample/jms/ibmmq/mq/controller/WebController.java
@@ -13,7 +13,7 @@ public class WebController {
 
     @ApiOperation(value = "To publish message to IBM MQ", nickname = "Sends a message to a specified queue.",
           notes = "Sends a UTF-8 encoded text message to an IBM MQ queue.")
-    @RequestMapping(value = "/produce" , method = RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/produce" , method = RequestMethod.POST, produces="MediaType.TEXT_PLAIN")
     @ResponseBody
     public String produce(@RequestParam("msg") String msg, @RequestParam("correlationId") String correlationId, @RequestParam("qName") String qName) {
         jmsClient.send(msg, correlationId, qName);
@@ -21,7 +21,7 @@ public class WebController {
     }
 
     @ApiOperation(value = "Receive", nickname = "Browses the next message from a specified queue.", notes = "Retrieves the next available text message from an IBM MQ queue.")
-    @RequestMapping(value = "/receive", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/receive", method = RequestMethod.GET, produces="MediaType.TEXT_PLAIN")
     @ResponseBody
     public String receive(@RequestParam("correlationId") String correlationId, @RequestParam("qName") String qName) {
     	return jmsClient.receive(correlationId, qName);
